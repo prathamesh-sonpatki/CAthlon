ff24c1587eae62fdddaa673ae69937b159239f1e
Prathamesh Sonpatki <csonpatki@gmail.com>
2013-02-06 14:54:06 +0530

    Started reviving this project for Melange 2013

--- questions/createtable.rb
+++ questions/createtable.rb
@@ -1,34 +1,41 @@
 require 'mysql'
+
 begin
-    con = Mysql.new 'localhost', 'root', 'prathamesh', 'CAthlon'
-#Create 6 tables for all levels of the contest!      
-con.query("Create Table Questions1 
+
+  db_name   = 'CAthlon'
+  db_host   = 'localhost'
+  user_name = 'root'
+  password  = 'prathamesh'
+
+  connection = Mysql.new(db_host, user_name, password)
+  connection.query("drop database if exists #{db_name}")
+  connection.query("create database #{db_name}")
+  connection.query("use #{db_name}")
+
+  #Create 6 tables for all levels of the connectiontest!
+
+  connection.query("Create Table Questions1
 	(id INT PRIMARY KEY AUTO_INCREMENT ,q_text varchar(500),option1 varchar(200),option2 varchar(200),option3 varchar(200),option4 varchar(200),answer varchar(10)) ")
 
-con.query("Create Table Questions2  
+  connection.query("Create Table Questions2
         (id INT PRIMARY KEY AUTO_INCREMENT ,q_text varchar(500),option1 varchar(200),option2 varchar(200),option3 varchar(200),option4 varchar(200),answer varchar(10)) ")
 
-con.query("Create Table Questions3 
+  connection.query("Create Table Questions3
         (id INT PRIMARY KEY AUTO_INCREMENT ,q_text varchar(500),option1 varchar(200),option2 varchar(200),option3 varchar(200),option4 varchar(200),answer varchar(10)) ")
 
-con.query("Create Table Questions4 
+  connection.query("Create Table Questions4
         (id INT PRIMARY KEY AUTO_INCREMENT ,q_text varchar(500),option1 varchar(200),option2 varchar(200),option3 varchar(200),option4 varchar(200),answer varchar(10)) ")
 
-con.query("Create Table Questions5 
+  connection.query("Create Table Questions5
         (id INT PRIMARY KEY AUTO_INCREMENT ,q_text varchar(500),option1 varchar(200),option2 varchar(200),option3 varchar(200),option4 varchar(200),answer varchar(10)) ")
 
-con.query("Create Table Questions6 
+  connection.query("Create Table Questions6
         (id INT PRIMARY KEY AUTO_INCREMENT ,q_text varchar(500),option1 varchar(200),option2 varchar(200),option3 varchar(200),option4 varchar(200),answer varchar(10)) ")
-
-  #   rs.each do |row|
-       # puts row.join("\n")
-   #  end
 
 rescue Mysql::Error => e
-    puts e.errno
-    puts e.error
+  puts e.errno
+  puts e.error
 
 ensure
-    con.close if con
+  connection.close if connection
 end
-
